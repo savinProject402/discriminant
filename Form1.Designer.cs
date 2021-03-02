f2968430d6e30756c4a147ee71eebc0e3fdd031d
savinProject402 <74914774+savinProject402@users.noreply.github.com>
2021-03-02 04:51:19 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace DZ_1_modul_1_WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Reshit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.number_A = new System.Windows.Forms.TextBox();
+            this.number_B = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.number_C = new System.Windows.Forms.TextBox();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reshit
+            // 
+            this.Reshit.Location = new System.Drawing.Point(59, 166);
+            this.Reshit.Name = "Reshit";
+            this.Reshit.Size = new System.Drawing.Size(75, 23);
+            this.Reshit.TabIndex = 0;
+            this.Reshit.Text = "count up";
+            this.Reshit.UseVisualStyleBackColor = true;
+            this.Reshit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X^2 +";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X +";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(392, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // number_A
+            // 
+            this.number_A.Location = new System.Drawing.Point(34, 40);
+            this.number_A.Name = "number_A";
+            this.number_A.Size = new System.Drawing.Size(50, 20);
+            this.number_A.TabIndex = 4;
+            this.number_A.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // number_B
+            // 
+            this.number_B.Location = new System.Drawing.Point(140, 40);
+            this.number_B.Name = "number_B";
+            this.number_B.Size = new System.Drawing.Size(48, 20);
+            this.number_B.TabIndex = 5;
+            this.number_B.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "= 0";
+            // 
+            // number_C
+            // 
+            this.number_C.Location = new System.Drawing.Point(247, 40);
+            this.number_C.Name = "number_C";
+            this.number_C.Size = new System.Drawing.Size(42, 20);
+            this.number_C.TabIndex = 7;
+            this.number_C.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 218);
+            this.Controls.Add(this.number_C);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.number_B);
+            this.Controls.Add(this.number_A);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Reshit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Reshit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox number_A;
+        private System.Windows.Forms.TextBox number_B;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox number_C;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_1_modul_1_WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                double number_A, number_B, number_C;
+
+                if (this.number_A.Text == "")
+                {
+                    number_A = 1;
+                }
+                else
+                {
+                    number_A = Convert.ToDouble(this.number_A.Text);
+                }
+
+                if (this.number_B.Text == "")
+                {
+                    number_B = 1;
+                }
+                else
+                {
+                    number_B = Convert.ToDouble(this.number_B.Text);
+                }
+                if (this.number_C.Text == "")
+                {
+                    number_C = 0;
+                }
+                else
+                {
+                    number_C = Convert.ToDouble(this.number_C.Text);
+                }
+
+                double Discriminant = number_B * number_B - 4 * number_A * number_C;
+
+                if (Discriminant < 0)
+                {
+                    MessageBox.Show("Дискриминант меньше нуля. Корней нет.");
+                }
+                while (Discriminant == 0)
+                {
+                    double x = (-number_B / (2 * number_A));
+                    MessageBox.Show("Дискриминант равен нулю. Корень равен " + x + "."); ;
+
+                }
+                if (Discriminant > 0)
+                {
+
+                    double x1 = Math.Round((-number_B - Math.Sqrt(Discriminant)) / (2 * number_A), 2);
+                    double x2 = Math.Round((-number_B + Math.Sqrt(Discriminant)) / (2 * number_A), 2);
+                    MessageBox.Show("Дискриминант равен " + Discriminant + ". Первый корень равен " + x1 + ". Второй корень равен " + x2 + ".");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input error, please try again (numbers only).  -----> \t Сlick OK and click button Refresh  on first form ");
+
+            }
+        }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+
+                foreach (Control c in Controls)
+                {
+                    if (c.GetType() == typeof(GroupBox))
+                        foreach (Control d in c.Controls)
+                            if (d.GetType() == typeof(TextBox))
+                                d.Text = string.Empty;
+
+                    if (c.GetType() == typeof(TextBox))
+                        c.Text = string.Empty;
+                }
+
+
+
+            }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        }
+    }
+
